6f106e51e4e1554675048e621327a6a5ce55121b
QuanZhi Huang <me@jackiehuang.ca>
2016-07-29 13:08:59 -0400

    Forms Created
    Book Flight, Customer Flight table, Customer List, Flight Info and
    Registration Form created

---
+++ SFlightBooking/SFlightBooking/CustomersList.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SFlightBooking {
+    /// <summary>
+    /// Interaction logic for CustomersList.xaml
+    /// </summary>
+    public partial class CustomersList : Window {
+        public CustomersList() {
+            InitializeComponent();
+        }
+    }
+}
