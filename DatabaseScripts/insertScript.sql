a620a9e8007ef528343ef34bf90aadd47f42f7f6
QuanZhi Huang <huangqu@sheridanc.on.ca>
2016-08-07 15:12:18 -0400

    Added insert Script

---
+++ DatabaseScripts/insertScript.sql
@@ -0,0 +1,47 @@
+INSERT INTO `sheridanproject`.`customer`
+(`uid`,
+`FirstName`,
+`LastName`,
+`Address`,
+`Phone`,
+`Gender`,
+`BirthDate`,
+`EnName`,
+`EnRelationship`,
+`EnPhone`) 
+VALUES 
+(null,"Jackie","Huang","Somewhere","123123","Male","2016-05-02","Sam Smith","Friend","911");
+
+
+INSERT INTO `sheridanproject`.`customer`
+(`uid`,
+`FirstName`,
+`LastName`,
+`Address`,
+`Phone`,
+`Gender`,
+`BirthDate`,
+`EnName`,
+`EnRelationship`,
+`EnPhone`) 
+VALUES 
+(null,"Obama","Barack","Whitehouse","4161102223","Male","1961-08-04","Michelle Obama","Spouse","4161122222");
+
+
+INSERT INTO `sheridanproject`.`customer`
+(`uid`,
+`FirstName`,
+`LastName`,
+`Address`,
+`Phone`,
+`Gender`,
+`BirthDate`,
+`EnName`,
+`EnRelationship`,
+`EnPhone`) 
+VALUES 
+(null,"Gates","Bill","Microsoft HQ","511100022","Male","1955-10-28","Melinda Gates","Spouse","5222110023");
+
+
+
+
