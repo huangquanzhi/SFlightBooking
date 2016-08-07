6ae0ad673b5ce9b3b6ad13c611503a66755565e3
QuanZhi Huang <huangqu@sheridanc.on.ca>
2016-08-06 20:00:28 -0400

    BookFlight List loaded
    + Book Flight form can now load flight and customer data
    + Flight Class added get and setter

--- SFlightBooking/SFlightBooking/BookFlight.xaml.cs
+++ SFlightBooking/SFlightBooking/BookFlight.xaml.cs
@@ -17,8 +17,98 @@ namespace SFlightBooking {
     /// Interaction logic for BookFlight.xaml
     /// </summary>
     public partial class BookFlight : Window {
+
+        List<Customer> customerList;
+        List<Flight> flightList;
+        int flightIndex = -1;
+        int customerIndex = -1;
+
         public BookFlight() {
             InitializeComponent();
+
+            // create and load data
+            initListView();
+            listviewLoad();
+            customerListLoad();
+        }
+
+        private void btn_bookFlight_Click(object sender, RoutedEventArgs e)
+        {
+            flightIndex = SelectedFlightIndex();
+            customerIndex = SelectedCustomerIndex();
+
+            if(flightIndex != -1 && customerIndex != -1)
+            {
+                // Flight and Customer Selected
+            } else
+            {
+                // Not selected
+                MessageBox.Show("Please select both flight and customer to book a flight!");
+            }
+        }
+
+        private void customerListLoad()
+        {
+            customerList = new List<Customer>();
+
+            // TODO: Load flights from database base on selected customer
+
+            lb_customers.Items.Add("Test");
+        }
+
+        private void listviewLoad()
+        {
+            flightList= new List<Flight>();
+
+            // TODO: Load flights from database base on selected customer
+
+            lv_flights.ItemsSource = flightList;
+        }
+
+        private void initListView()
+        {
+            GridView view = new GridView();
+
+            GridViewColumn col1 = new GridViewColumn();
+            col1.Header = "Departure";
+            col1.DisplayMemberBinding = new Binding("Departure");
+            col1.Width = 100;
+            view.Columns.Add(col1);
+
+            GridViewColumn col2 = new GridViewColumn();
+            col2.Header = "Destination";
+            col2.DisplayMemberBinding = new Binding("Destination");
+            col2.Width = 100;
+            view.Columns.Add(col2);
+
+            GridViewColumn col3 = new GridViewColumn();
+            col3.Header = "Date";
+            col3.DisplayMemberBinding = new Binding("Date");
+            col3.Width = 100;
+            view.Columns.Add(col3);
+
+            GridViewColumn col4 = new GridViewColumn();
+            col4.Header = "AvailbleSeats";
+            col4.DisplayMemberBinding = new Binding("AvailbleSeats");
+            col4.Width = 100;
+            view.Columns.Add(col4);
+            // create view
+            lv_flights.View = view;
+        }
+
+        private int SelectedFlightIndex()
+        {
+            return lv_flights.SelectedIndex;
+        }
+
+        private int SelectedCustomerIndex()
+        {
+            return lb_customers.SelectedIndex;
+        }
+
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- SFlightBooking/SFlightBooking/Flight.cs
+++ SFlightBooking/SFlightBooking/Flight.cs
@@ -19,5 +19,120 @@ namespace SFlightBooking
         private int availbleSeats;
         private string status;
 
+        public Flight(string flightID, string departure, string destination, string duration, string date, string time, int availbleSeats, string status)
+        {
+            this.flightID = flightID;
+            this.departure = departure;
+            this.destination = destination;
+            this.duration = duration;
+            this.date = date;
+            this.time = time;
+            this.availbleSeats = availbleSeats;
+            this.status = status;
+        }
+
+        public string FlightID
+        {
+            get
+            {
+                return flightID;
+            }
+
+            set
+            {
+                flightID = value;
+            }
+        }
+
+        public string Departure
+        {
+            get
+            {
+                return departure;
+            }
+
+            set
+            {
+                departure = value;
+            }
+        }
+
+        public string Destination
+        {
+            get
+            {
+                return destination;
+            }
+
+            set
+            {
+                destination = value;
+            }
+        }
+
+        public string Duration
+        {
+            get
+            {
+                return duration;
+            }
+
+            set
+            {
+                duration = value;
+            }
+        }
+
+        public string Date
+        {
+            get
+            {
+                return date;
+            }
+
+            set
+            {
+                date = value;
+            }
+        }
+
+        public string Time
+        {
+            get
+            {
+                return time;
+            }
+
+            set
+            {
+                time = value;
+            }
+        }
+
+        public int AvailbleSeats
+        {
+            get
+            {
+                return availbleSeats;
+            }
+
+            set
+            {
+                availbleSeats = value;
+            }
+        }
+
+        public string Status
+        {
+            get
+            {
+                return status;
+            }
+
+            set
+            {
+                status = value;
+            }
+        }
     }
 }
